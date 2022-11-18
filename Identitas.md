6d6060904801db5eafc74618dd219e2a76422b06
BremaAlfansi <118526912+BremaAlfansi@users.noreply.github.com>
2022-11-18 23:00:09 +0700

    First Submission by Brema

---
+++ Identitas.md
@@ -0,0 +1,4 @@
+# First submission by Brema Afansi
+
+## Screenshot
+![screenshot](screenshot.jpg)
---
+++ main.py
@@ -0,0 +1,4 @@
+print("Nama:Brema Alfansi Ginting")
+print("Jurusan: FTI-G ")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin belajar lebih banyak tentang bagian-bagian pemrograman dan menambah relasi teman seangkatan dan kakak tingkat)                                                                      ")
